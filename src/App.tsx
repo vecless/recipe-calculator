03b665ffc055d35f55a68b041f53d09ec4530873
ingi h <35065616+vecless@users.noreply.github.com>
2024-07-02 12:20:46 -0500

    Add multiple fairly nice things
    - Move most app logic from App to AppActivityRegion
    --- Figuring out how to flow data between "app activity region" and top bar (through common parent of `App` was getting really tedious)
    --- This is probably a good way to do things from the beginning since the scope of the app is deliberately not so huge
    --- Arguably it further makes sense to delete App now, but I am lazy :P
    - Actually add About, Import/Export panels, that MOSTLY work (as far as I can tell)
    - Add confirm/warn on reload
    - This is too many things for one commit arguably, but oh well
    - This creates some vague concerns for me about "creating too many object"/memory management type ideas but I don't know enough about web to know if these are valid or imagined
    - :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import Toolbar from '@mui/material/Toolbar';
 
 import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
-import { Box, Button, ButtonGroup, Container, IconButton, Tooltip } from '@mui/material';
+import { Box, Button, ButtonGroup, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Tooltip } from '@mui/material';
 import Blender from '@mui/icons-material/Blender'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { HorizontalRule, InfoRounded, ReportProblemRounded, UploadFileRounded } from '@mui/icons-material';
 
 import * as LocalData from './utils';
 import RoundedBox from './components/RoundedBox';
 import Factor, { DEFAULT_FACTOR_DATA, FactorData } from './components/Factor';
 import { DataGrid } from '@mui/x-data-grid';
-import { DATAGRIDCOLS, generateGridRowsProp } from './generateDataGrid';
+import { DATAGRIDCOLS, generateGridRowsProp } from './DataGrid';
 import TopForm, { TopFormData } from './components/TopForm';
+import { jsonToURI } from './JsonUtils';
+import VisuallyHiddenInput from './components/VisuallyHiddenInput';
 
 /**
  * App Activity Region Props
@@ -34,6 +37,8 @@ class AppActivityRegion extends React.Component<AARProps, {
   factorForms: JSX.Element[],
   // The last valid submission from each child form, for each child form
   lastSubmission: FactorData[],
+  // Top form
+  topForm: JSX.Element,
   water: number,
 }> {
   private childFormHandler = (formKey: number) => {
@@ -47,7 +52,26 @@ class AppActivityRegion extends React.Component<AARProps, {
   }
 
   private newEntryPanel = (index: number): JSX.Element => {
-    return <Factor key={index} guidingHand={this.childFormHandler(index)} />;
+    return <Factor key={"pan" + index.toString()} guidingHand={this.childFormHandler(index)} />;
+  }
+
+  private newEntryPanelWithVals = (index: number, initialData: FactorData) => {
+    return <Factor key={"pan" + index.toString()} guidingHand={this.childFormHandler(index)}
+      type={initialData.type}
+      subtype={initialData.subtype}
+      mL={initialData.mL}
+      g={initialData.g}
+    />;
+  }
+
+  componentDidMount() {
+    window.onbeforeunload = function (e: BeforeUnloadEvent) {
+      e.preventDefault();
+    };
+  }
+
+  componentWillUnmount() {
+    window.onbeforeunload = null;
   }
 
   constructor(props: AARProps) {
@@ -58,6 +82,7 @@ class AppActivityRegion extends React.Component<AARProps, {
       plausibleEntries: 1,
       factorForms: [this.newEntryPanel(0)],
       lastSubmission: [DEFAULT_FACTOR_DATA],
+      topForm: <TopForm guidingHand={this.topFormHandler} water={0} />,
       water: 0, // 0 ml is a sensible default
     };
   }
@@ -138,27 +163,241 @@ class AppActivityRegion extends React.Component<AARProps, {
 
   private topFormHandler = (values: TopFormData) => {
     this.setState({ water: values.water });
-    console.log("Updated water to " + values.water);
     this.forceUpdate();
   }
 
   private renderTopRegion() {
     return (<div>
-      <TopForm guidingHand={this.topFormHandler} water={0} />
+      {this.state.topForm}
     </div>);
   }
 
+
+  /**
+   * Save the state of the app, ignoring unnecessary JSX.Elements which are regenerated on import anyways
+   * @returns the state, "minus" the "useless" forms properties
+   */
+  private saveForExport = (): object => {
+    // Honestly, not sure if destructuring here is "good", but surely it's not that bad.
+    const { factorForms, topForm, ...stateMinusForms } = this.state;
+    return stateMinusForms;
+  }
+
+
+  /**
+   * Provides a more "checked/safe" restore from any object (but really, should be the state value)
+   * @param obj the state object
+   * @returns void
+   */
+  private cautiousRestore = (obj: any): void => {
+    // exclude annotation probably not needed anymore
+    const ensureHas = (item: Exclude<any, string>, str: string): boolean => Object.prototype.hasOwnProperty.call(item, str);
+    if (!(ensureHas(obj, "plausibleEntries") && ensureHas(obj, "water") && ensureHas(obj, "lastSubmission"))) return;
+
+    const count: number = obj.plausibleEntries;
+    this.setState({ plausibleEntries: count, water: obj.water, topForm: <TopForm guidingHand={this.topFormHandler} water={obj.water} /> });
+
+    // Set last submissions and factor forms according to specification (suppose data is "probably valid")
+    this.state.factorForms.length = this.state.lastSubmission.length = count;
+
+    for (let i = 0; i < count; ++i) {
+      if (
+        !(i < 0 || i > count) &&
+        ensureHas(obj.lastSubmission, "length") &&
+        obj.lastSubmission.length == count &&
+        ["type", "subtype", "mL", "g"].every(key => ensureHas(obj.lastSubmission[i], key))
+      ) {
+        this.state.lastSubmission[i] = obj.lastSubmission[i] as FactorData;
+        this.state.factorForms[i] = this.newEntryPanelWithVals(i, this.state.lastSubmission[i]);
+      } else {
+        // Could not determine desired data
+        this.state.lastSubmission[i] = DEFAULT_FACTOR_DATA;
+        this.state.factorForms[i] = this.newEntryPanel(i);
+      }
+    }
+  }
+
+  /**
+   * Render the app activity.
+   */
   render() {
-    return (<div>
-      <Box sx={{ display: 'grid', gridAutoFlow: 'dense', gridTemplateColumns: 'repeat(2, 1fr)', gap: '1' }}>
-        {/* lol this is so fragile */}
-        <RoundedBox sx={{ gridColumn: 'span 3', gridRow: '1' }}> {this.renderTopRegion()} </RoundedBox>
-        <RoundedBox sx={{ gridColumn: '1', gridRow: '2 / 3', maxWidth: '67vw' }}> {this.renderFormRegion()} </RoundedBox>
-        <RoundedBox sx={{ gridColumn: '2', gridRow: '2 / 3', maxWidth: '50vw' }}> {this.renderSidebarRegion()} </RoundedBox>
-      </Box>
-    </div>);
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Blender icon" disableRipple>
+              <Blender />
+            </IconButton>
+            <Typography variant="h4" className="title" noWrap>
+              Renal Formula Calculator
+            </Typography>
+
+            <div style={{ marginLeft: 'auto', marginRight: 0 }}>
+              <ImportExportDialog
+                getStateObject={this.saveForExport}
+                useImportedStateObject={this.cautiousRestore}
+              />
+              <Tooltip title="Report a problem">
+                <IconButton color="inherit" aria-label="Simple icon button"
+                  href="mailto:academe.ragouts_0l@icloud.com?subject=Renal%20Formula%20Calculator:%20[DESCRIBE%20THE%20ISSUE]&body=%0A%0A(Note:%20Don't%20worry%20if%20the%20recipient%20address%20looks%20weird,%20it%20is%20anonymized%20for%20privacy%20and%20automatic%20filtering)"
+                  target="_blank"> <ReportProblemRounded />
+                </IconButton>
+              </Tooltip>
+              <AboutDialog />
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <Container maxWidth={false}>
+          <p />
+          <div>
+            <Box sx={{ display: 'grid', gridAutoFlow: 'dense', gridTemplateColumns: 'repeat(2, 1fr)', gap: '1' }}>
+              {/* lol this is so fragile */}
+              <RoundedBox sx={{ gridColumn: 'span 3', gridRow: '1' }}> {this.renderTopRegion()} </RoundedBox>
+              <RoundedBox sx={{ gridColumn: '1', gridRow: '2 / 3', maxWidth: '67vw' }}> {this.renderFormRegion()} </RoundedBox>
+              <RoundedBox sx={{ gridColumn: '2', gridRow: '2 / 3', maxWidth: '50vw' }}> {this.renderSidebarRegion()} </RoundedBox>
+            </Box>
+          </div>
+
+        </Container>
+      </div>
+    );
   }
 }
+
+interface ImportExportDialogProps {
+  getStateObject: () => any;
+  useImportedStateObject: (obj: any) => void;
+}
+
+const ImportExportDialog: React.FC<ImportExportDialogProps> = ({ getStateObject, useImportedStateObject }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const [fileContent, setFileContent] = React.useState(null);
+  const [fileName, setFileName] = React.useState("");
+
+  const fileInput = React.useRef(null);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      try {
+        if (event.target) {
+          const json = JSON.parse(event.target.result as string);
+          // note: this is not just the filename
+          setFileName(file.name + " (" + file.size + " B)");
+          setFileContent(json);
+        }
+      } catch (error) {
+        console.error('Error parsing JSON file: ', error);
+      }
+    };
+
+    reader.readAsText(file);
+  };
+
+  return (
+    <>
+      <Tooltip title="Import/Export">
+        <IconButton color="inherit" aria-label="Simple icon button" onClick={() => setOpen(true)} > <UploadFileRounded /> </IconButton>
+      </Tooltip>
+
+      <Dialog open={open} onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Import / Export"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Importing or exporting your data means you can save your working session and return to it later. A small text file is downloaded to or uploaded from your computer, don't lose it!
+          </DialogContentText>
+
+          <br />
+
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+          >
+            Import (upload file)
+            <VisuallyHiddenInput type="file" ref={fileInput} onChange={handleFileUpload} accept=".json" />
+          </Button>
+          {fileContent && " " + fileName}
+
+          {/* for debugging */}
+          {/* {fileContent && <pre>{JSON.stringify(fileContent, null, 2)}</pre>} */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => {
+            if (fileContent) {
+              useImportedStateObject(fileContent);
+            }
+          }} disabled={fileContent == null}>
+            Import loaded session
+          </Button>
+          <Button onClick={() => { }} href={jsonToURI(getStateObject())} download={"rfc_session.json"}>
+            Export current data
+          </Button>
+          <Button onClick={() => setOpen(false)} autoFocus>
+            Close popup
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+
+const AboutDialog = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <Tooltip title="About this site">
+        <IconButton color="inherit" aria-label="Simple icon button" onClick={() => setOpen(true)} > <InfoRounded /> </IconButton>
+      </Tooltip>
+
+      <Dialog open={open} onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"About"}
+        </DialogTitle>
+        <DialogContent id="alert-dialog-description">
+          <DialogContentText>
+            This site helps calculate the nutrition of formulas for pediatric patients on dialysis and children with kidney disease.
+            The interface is similar to an Excel spreadsheet, and hopefully carries lower risk of misuse or data damage.
+            <br />
+            This website was created by <a href="https://www.linkedin.com/in/ingibhelgason" target="_blank">Ingi Helgason</a> using React.
+          </DialogContentText>
+          <DialogTitle> Disclaimer </DialogTitle>
+          <DialogContentText>
+
+            This site is a work-in-progress and intended for informational purposes. It is provided 'as-is' and without any explicit claims of quality. Whether you are a healthcare professional or a patient, exercise caution, rely on your judgment, and consult authoritative resources when necessary.
+            The nutritional data comes from original sources and has undergone professional review. While believed to be accurate, it is not currently published and may be subject to further review.
+            <br /> This disclaimer has not been reviewed by a legal professional.
+          </DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} autoFocus>
+            Close popup
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
 
 /**
  * This defines the main application component.
@@ -167,39 +406,6 @@ class AppActivityRegion extends React.Component<AARProps, {
  */
 export default function App() {
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton color="inherit" aria-label="Blender icon" disableRipple>
-            <Blender />
-          </IconButton>
-          <Typography variant="h4" className="title" noWrap>
-            Renal Formula Calculator
-          </Typography>
-
-          <div style={{ marginLeft: 'auto', marginRight: 0 }}>
-            <Tooltip title="Import/Export">
-              <IconButton color="inherit" aria-label="Simple icon button"> <UploadFileRounded /> </IconButton>
-            </Tooltip>
-            <Tooltip title="Report a problem">
-              <IconButton color="inherit" aria-label="Simple icon button"
-                href="mailto:academe.ragouts_0l@icloud.com?subject=Renal%20Formula%20Calculator:%20[DESCRIBE%20THE%20ISSUE]&body=%0A%0A(Note:%20Don't%20worry%20if%20the%20recipient%20address%20looks%20weird,%20it%20is%20anonymized%20for%20privacy%20and%20automatic%20filtering)"
-                target="_blank"> <ReportProblemRounded />
-              </IconButton>
-            </Tooltip>
-
-            <Tooltip title="About this site">
-              <IconButton color="inherit" aria-label="Simple icon button"> <InfoRounded /> </IconButton>
-            </Tooltip>
-          </div>
-        </Toolbar>
-      </AppBar>
-
-      <Container maxWidth={false}>
-        <p />
-        <AppActivityRegion />
-
-      </Container>
-    </div>
+    <AppActivityRegion />
   );
 }
